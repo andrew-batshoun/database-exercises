4b23f1c55bd9460a61331d03494b3b50fe255c3f
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-01-25 09:54:43 -0600

    updated sql code to drop table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,11 @@
 USE codeup_test_db;
-CREATE TABLE albums (
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(50) NOT NULL,
                         name  VARCHAR(150) NOT NULL,
                         release_date DATE NOT NULL,
-                        sales FLOAT NOT NULL,
+                        sales INT NOT NULL,
                         genre VARCHAR(50) NOT NULL,
                         PRIMARY KEY (id)
 );
