7a51b451a29bc1a61bb053c09eae655349dfaab1
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-01-25 10:00:26 -0600

    updated sql code

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(50) NOT NULL,
                         name  VARCHAR(150) NOT NULL,
-                        release_date DATE NOT NULL,
+                        release_date INT NOT NULL,
                         sales INT UNSIGNED NOT NULL,
                         genre VARCHAR(50) NOT NULL,
                         PRIMARY KEY (id)
