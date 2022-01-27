e458a7be3ee36f992a99689d70a27f8d19ee9fa8
Andrawes Batshoun <andrawes.batshoun@gmail.com>
2022-01-27 16:17:48 -0600

    completed alias exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,7 +2,7 @@ use employees;
 
 SELECT DISTINCT title from titles;
 
-SELECT last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY last_name;
+SELECT last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY last_name ORDER BY last_name;
 
 SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY last_name, first_name;
 
@@ -10,4 +10,4 @@ SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_nam
 
 SELECT COUNT(last_name), last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
-SELECT COUNT(gender), gender FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
+SELECT COUNT(*), gender FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') GROUP BY gender;
